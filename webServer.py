512a84d0cb1ddb21cccdc277a93a890b60bc61e7
Chi-Jonathan <89598430+Chi-Jonathan@users.noreply.github.com>
2024-09-25 17:14:31 -0700

    got rid of extra headers

--- webServer.py
+++ webServer.py
@@ -35,30 +35,30 @@ def webServer(port=13331):
 
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
       #Fill in start 
-      status_response = b"HTTP/1.1 200 OK\r\n"
+      # status_response = b"HTTP/1.1 200 OK\r\n"
 
       #Content-Type is an example on how to send a header as bytes. There are more!
       outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
 
-      server_response = b"Server: Python Web Server\r\n\r\n"
+      # server_response = b"Server: Python Web Server\r\n\r\n"
 
       # connection_response = b"Connection: close\r\n\r\n"
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
 
-      response = status_response + outputdata + server_response
+      # response = status_response + outputdata + server_response
 
       #Fill in end
                
       for i in f: #for line in file
       #Fill in start - append your html file contents #Fill in end
-        response += i
+        outputdata += i
 
         
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
 
       # Fill in start
-      connectionSocket.send(response)
+      connectionSocket.send(outputdata)
 
       # Fill in end
         
@@ -68,11 +68,11 @@ def webServer(port=13331):
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
-      status_response = b"HTTP/1.1 404 Not Found\r\n"
-      content_type = b"Content-Type: text/html; charset=UTF-8\r\n"
-      server_response = b"Server: Python Web Server\r\n\r\n"
-      response = status_response + content_type + server_response
-      connectionSocket.send(response)
+      # status_response = b"HTTP/1.1 404 Not Found\r\n"
+      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      # server_response = b"Server: Python Web Server\r\n\r\n"
+      # response = status_response + outputdata + server_response
+      connectionSocket.send(outputdata)
       #Fill in end
 
 
